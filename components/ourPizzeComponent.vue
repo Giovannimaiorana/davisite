9191a8e332032336e88211e8b57e4a2276844155
Giovannimaiorana <giovanni.maiorana.14@gmail.com>
2025-05-17 01:25:44 +0200

    aggiunto carosello con animazioni

--- components/ourPizzeComponent.vue
+++ components/ourPizzeComponent.vue
@@ -1,4 +1,111 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { gsap } from 'gsap'
+interface Pizza {
+  name: string
+  image: string
+}
+
+
+interface Pasta {
+  name: string
+  image: string
+}
+const pizzas: Pizza[] = [
+  { name: 'ROMANA', image: '/img/pizza1.svg' },
+  { name: 'MARGHERITA', image: '/img/pizza2.svg' },
+  { name: 'BELLA FRESCA', image: '/img/pizza3.svg' },
+  { name: 'NORMA', image: '/img/pizza4.svg' },
+  { name: 'TONNO', image: '/img/pizza5.svg' },
+  { name: 'MORTAZZA', image: '/img/pizza6.svg' },
+  { name: 'SALMONE', image: '/img/pizza7.svg' },
+  { name: 'ORTOLANA', image: '/img/pizza8.svg' },
+  { name: 'DAVI', image: '/img/pizza9.svg' },
+  { name: 'MARINARA', image: '/img/pizza10.svg' },
+  { name: 'DIAVOLA', image: '/img/pizza11.svg' },
+  
+]
+
+const currentIndex = ref(0)
+
+const currentPizza = computed(() => pizzas[currentIndex.value])
+const totalPizzas = pizzas.length
+
+function nextPizza() {
+  currentIndex.value = (currentIndex.value + 1) % totalPizzas
+}
+
+function prevPizza() {
+  currentIndex.value = (currentIndex.value - 1 + totalPizzas) % totalPizzas
+}
+//PASTA 
+const pastas: Pasta[] = [
+  { name: 'LASAGNA', image: '/img/pasta1.svg' },
+  { name: 'AMATRICIANA', image: '/img/pasta2.svg' },
+  { name: 'CARBONARA', image: '/img/pasta3.svg' },
+  { name: 'GNOCCHI ALLA SORRENTINA', image: '/img/pasta4.svg' },
+  { name: 'TARTURFO & FUNGHI', image: '/img/pasta5.svg' },
+]
+const currentIndexPasta = ref(0)
+
+const currentPasta = computed(() => pastas[currentIndexPasta.value])
+const totalPasta = pastas.length
+
+function nextPasta() {
+    currentIndexPasta.value = (currentIndexPasta.value + 1) % totalPasta
+}
+
+function prevPasta() {
+    currentIndexPasta.value = (currentIndexPasta.value - 1 + totalPasta) % totalPasta
+}
+//categoria selezionata
+const selectedCategory = ref<'pizza' | 'pasta'>('pizza');
+console.log('categoria selezionata ',selectedCategory)
+function selectCategory(category: 'pizza' | 'pasta') {
+  selectedCategory.value = category;
+}
+//animazione
+const pizzaImage = ref<HTMLElement | null>(null)
+const pastaImage = ref<HTMLElement | null>(null)
+
+
+const pizzaName = ref<HTMLElement | null>(null)
+const pastaName = ref<HTMLElement | null>(null)
+    watch([currentPizza, currentPasta, selectedCategory], async () => {
+  await nextTick()
+
+  const imageEl = selectedCategory.value === 'pizza' ? pizzaImage.value : pastaImage.value
+  const textEl = selectedCategory.value === 'pizza' ? pizzaName.value : pastaName.value
+
+  if (imageEl) {
+    gsap.fromTo(
+      imageEl,
+      { opacity: 0, scale: 0.95, y: 30 },
+      { opacity: 1, scale: 1, y: 0, duration: 0.6, ease: 'sine.out' }
+    )
+  }
+
+  if (textEl) {
+    gsap.fromTo(
+      textEl,
+      { opacity: 0, y: 15 },
+      { opacity: 1, y: 0, duration: 0.5, ease: 'power3.out', delay: 0.1 }
+    )
+  }
+})
+// Anima il cambio di immagine con GSAP
+watch([currentPizza, currentPasta, selectedCategory], async () => {
+  await nextTick()
+
+  const el = selectedCategory.value === 'pizza' ? pizzaImage.value : pastaImage.value
+  if (!el) return
+
+  gsap.fromTo(
+    el,
+    { opacity: 0, scale: 0, y: 20 },
+    { opacity: 1, scale: 1, y: 0, duration: 0.8, ease: 'power3.out' }
+  )
+})
+</script>
 
 <template> 
     <div class="wrapperPiatti telephone">
@@ -13,14 +120,25 @@
             </div>
             <div class="containerBottom">
                 <div class="containerNumber">
-                    <p class="font-dav text-xl font-bold">01</p>
+                    <button v-if="selectedCategory=== 'pizza'" @click="prevPizza" class="cursor-pointer">&lt;</button>
+                    <button v-if="selectedCategory=== 'pasta'" @click="prevPasta" class="cursor-pointer">&lt;</button>
+                    <p v-if="selectedCategory=== 'pizza'"  class="font-dav text-xl font-bold">{{ String(currentIndex + 1).padStart(2, '0') }}</p>
+                    <p v-if="selectedCategory=== 'pasta'"  class="font-dav text-xl font-bold">{{ String(currentIndexPasta + 1).padStart(2, '0') }}</p>
                 </div>
                 <div class="containerLine">
 
                 </div>
                 <div class="containerNumber">
-                    <p class="font-dav text-xl font-bold">09</p>
+                    <p v-if="selectedCategory=== 'pizza'" class="font-dav text-xl font-bold">{{ String(totalPizzas).padStart(2, '0') }}</p>
+                    <p v-if="selectedCategory=== 'pasta'" class="font-dav text-xl font-bold">{{ String(totalPasta).padStart(2, '0') }}</p>
+                    <button v-if="selectedCategory=== 'pizza'" @click="nextPizza" class="cursor-pointer">&gt;</button>
+                    <button v-if="selectedCategory=== 'pasta'" @click="nextPasta" class="cursor-pointer">&gt;</button>
+                    
                 </div> 
+                <div class="flex gap-2 mt-2">
+
+    
+        </div>
 
             </div>
         </div>
@@ -30,15 +148,20 @@
              
             </div>
             <div class="containerRightRight">
-                <div class="containerRightRightTop">
-                    <p class="font-dav sp text-3xl lg:text-4xl xl:text-5xl font-bold text-right active">PIZZAS</p>
-                    <p class="font-dav sp text-xl text-right">PLATS DE PATES</p>
-                    <p class="font-dav sp text-xl text-right">DESSERTS</p>
-
+                <div v-if="selectedCategory==='pizza'" class="containerRightRightTop">
+                    <p class="font-dav sp text-3xl lg:text-4xl xl:text-5xl font-bold text-right active cursor-pointer" @click="selectCategory('pizza')" >PIZZAS</p>
+                    <p class="font-dav sp text-xl text-right cursor-pointer" @click="selectCategory('pasta')">PLATS DE PATES</p>
+                </div>
+                <div v-if="selectedCategory==='pasta'" class="containerRightRightTop">
+                    <p class="font-dav sp  text-3xl lg:text-4xl xl:text-5xl text-right cursor-pointer" @click="selectCategory('pasta')">PLATS DE PATES</p>
+                    <p class="font-dav sp text-xl font-bold text-right active cursor-pointer" @click="selectCategory('pizza')" >PIZZAS</p>
+                 
                 </div>
                 <div class="containerRightRightBottom">
-                    <img class="pizza" src="/img/pizza1.svg" alt="">
-                    <p class="nomePizza text-3xl lg:text-4xl xl:text-6xl ml-3 text-white font-dav sp">MARGHERITA</p>
+                    <img   ref="pizzaImage"  v-if="selectedCategory=== 'pizza'" class="pizza" :src="currentPizza.image"alt="">
+                    <img   ref="pastaImage" v-if="selectedCategory=== 'pasta'" class="pizza" :src="currentPasta.image"alt="">
+                    <p   ref="pizzaName" v-if="selectedCategory=== 'pizza'" class="nomePizza text-3xl lg:text-4xl xl:text-6xl ml-3 text-white font-dav sp"> {{ currentPizza.name }}</p>
+                    <p    ref="pastaName"v-if="selectedCategory=== 'pasta'" class="nomePizza text-3xl lg:text-4xl xl:text-6xl ml-3 text-white font-dav sp"> {{ currentPasta.name }}</p>
                 </div>
             </div>
 
@@ -132,6 +255,12 @@
     display: flex;
     align-items: end;
 
+}
+.containerNumber{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
 }
 .pizza{
     top: -150px;
